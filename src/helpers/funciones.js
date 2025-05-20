c05c962ad7003c2ea116394819d8bdccc956b8de
Anselmo Ortega <lfkanselmo@gmail.com>
2025-05-20 10:39:57 -0500

    feat se implementa el método DELETE

--- src/helpers/funciones.js
+++ src/helpers/funciones.js
@@ -1,33 +1,62 @@
 import Swal from "sweetalert2";
-export function alertaRedireccion(redireccion, mensaje, ruta){
-    let timerInterval;
-    Swal.fire({
-        title: mensaje,
-        html: "Será redireccionado en <b></b> milisegundos.",
-        timer: 1500,
-        timerProgressBar: true,
-        didOpen: () => {
-            Swal.showLoading();
-            const timer = Swal.getPopup().querySelector("b");
-            timerInterval = setInterval(() => {
-            timer.textContent = `${Swal.getTimerLeft()}`;
-            }, 100);
-        },
-        willClose: () => {
-            clearInterval(timerInterval);
-            redireccion(ruta)
-        }
-    })  
+export function alertaRedireccion(redireccion, mensaje, ruta) {
+  let timerInterval;
+  Swal.fire({
+    title: mensaje,
+    html: "Será redireccionado en <b></b> milisegundos.",
+    timer: 1500,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading();
+      const timer = Swal.getPopup().querySelector("b");
+      timerInterval = setInterval(() => {
+        timer.textContent = `${Swal.getTimerLeft()}`;
+      }, 100);
+    },
+    willClose: () => {
+      clearInterval(timerInterval);
+      redireccion(ruta);
+    },
+  });
 }
 
-export function alertaGeneral(titulo, mensaje, icono){
-    Swal.fire({
-        icon: icono,
-        title: titulo,
-        text: mensaje
-      });
+export function alertaGeneral(titulo, mensaje, icono) {
+  Swal.fire({
+    icon: icono,
+    title: titulo,
+    text: mensaje,
+  });
 }
-export function generarToken(){
-    let token = "token_" + Math.random().toString(36).substring(2) + "-" + Math.random().toString(36).substring(2);
-    return token;
+
+export function alertaConfirmar(id, apiEnvios, getEnvios) {
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      fetch(apiEnvios + "/id" + id, {
+      method: "DELETE"
+    }).then(() => {
+      getEnvios()
+    })
+      Swal.fire({
+        title: "Deleted!",
+        text: "Your file has been deleted.",
+        icon: "success",
+      });
+    }
+  });
+}
+export function generarToken() {
+  let token =
+    "token_" +
+    Math.random().toString(36).substring(2) +
+    "-" +
+    Math.random().toString(36).substring(2);
+  return token;
 }
