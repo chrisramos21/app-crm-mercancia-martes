33c789675b8b471142f085045ff49eeac81d4fab
Anselmo Ortega <lfkanselmo@gmail.com>
2025-05-27 10:13:17 -0500

    feat implementación del método PATCH

--- src/pages/EditarEnvio.jsx
+++ src/pages/EditarEnvio.jsx
@@ -26,15 +26,51 @@ const EditarEnvio = () => {
     getEnvioId();
   }, []);
 
-  function editarEnvio() {}
+  function editarEnvio() {
+    let envioEditado = {
+      idEnvio: envio,
+      producto: producto,
+      destino: destino
+    };
+    fetch(apiEnvio + id, {
+      method: "PATCH",
+      body: JSON.stringify(envioEditado),
+    })
+      .then(() =>
+        alertaRedireccion(
+          redireccion,
+          "Registro editado correctamente",
+          "/home/envios"
+        )
+      )
+      .catch(() => alertaGeneral("error", "No se pudo editar", "error"));
+  }
 
   return (
     <form className="form">
       Sign Up
-      <input value={envio} type="text" className="input" placeholder="Id envio" />
-      <input value={producto} type="text" className="input" placeholder="Producto" />
-      <input value={destino} type="text" className="input" placeholder="Destino" />
-      <button type="button">Submit</button>
+      <input
+        onChange={(e) => setEnvio(e.target.value)}
+        value={envio}
+        type="text"
+        className="input"
+        placeholder="Id envio"
+      />
+      <input
+        onChange={(e) => setProducto(e.target.value)}
+        value={producto}
+        type="text"
+        className="input"
+        placeholder="Producto"
+      />
+      <input
+        onChange={(e) => setDestino(e.target.value)}
+        value={destino}
+        type="text"
+        className="input"
+        placeholder="Destino"
+      />
+      <button onClick={editarEnvio} type="button">Submit</button>
     </form>
   );
 };
